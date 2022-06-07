b19b706664fb3e20ae6f357a1b8e054b075569e7
marcoslsouza <marcoslsouzadf@gmail.com>
2022-06-06 22:47:36 -0300

    first commit

---
+++ src/main/java/com/github/marcoslsouza/MarcoslsouzaApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcoslsouza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcoslsouzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcoslsouzaApplication.class, args);
+	}
+
+}
