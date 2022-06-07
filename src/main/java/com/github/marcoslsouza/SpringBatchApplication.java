534ffe48c6a9ea206883b84b08732f396ee9ecc6
marcoslsouza <marcoslsouzadf@gmail.com>
2022-06-07 01:03:42 -0300

    Main file settings

--- src/main/java/com/github/marcoslsouza/SpringBatchApplication.java
+++ src/main/java/com/github/marcoslsouza/SpringBatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarcoslsouzaApplication {
+public class SpringBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarcoslsouzaApplication.class, args);
+		SpringApplication.run(SpringBatchApplication.class, args);
 	}
 
 }
